29210b16869e491e88ff21eb90e0ff6368343649
Mauricio J. Monta <mauriciomontadg@gmail.com>
2022-03-26 18:44:59 -0300

    home section modified

---
+++ script.js
@@ -0,0 +1,6 @@
+const menu = document.getElementById('nav')
+const navigate = document.getElementById('navigate');
+
+menu.addEventListener('mouseenter', function(e) {
+    navigate.classList.add('opacity0');
+})
